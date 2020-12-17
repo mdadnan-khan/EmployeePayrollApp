8dcf62e3159a9fba4c1192af0c522694be89752e
MD ADNAN KHAN <adnan1990877@gmail.com>
2020-12-18 03:39:59 +0530

    [Adnan]ADDED: code to establish connection with mysql repository

--- src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -1,36 +1,56 @@
 package com.bridgelabz.employeepayrollapp.model;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.persistence.JoinColumn;
+
+import org.springframework.data.annotation.Id;
+
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
 
-public class EmployeePayrollData {
+import lombok.Data;
+
+@Entity
+@Table(name = "employee_payroll")
+public @Data class EmployeePayrollData {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name = "employee_id")
 	private int employeeId;
+	
+	@Column(name="name")
 	private String name;
 	private long salary;
+	private String gender;
+	private String startDate;
+	private String note;
+	private String profilePic;
 	
-	public EmployeePayrollData(int empId, EmployeePayrollDTO empPayrollDTO) {
-		this.employeeId = empId;
+	@ElementCollection
+	@CollectionTable(name = "employee_department", joinColumns = @JoinColumn(name = "id"))
+	@Column(name = "department")
+	private List<String> departments;
+	
+	public EmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
+		this.updateEmployeePayrollData(empPayrollDTO);
+	}
+	
+	public void updateEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
 		this.name = empPayrollDTO.name;
 		this.salary = empPayrollDTO.salary;
+		this.gender = empPayrollDTO.gender;
+		this.note = empPayrollDTO.note;
+		this.startDate = empPayrollDTO.startDate;
+		this.profilePic = empPayrollDTO.profilePic;
+		this.departments = empPayrollDTO.department;
 	}
-	
-	public int getEmployeeId() {
-		return employeeId;
-	}
-	public void setEmployeeId(int employeeId) {
-		this.employeeId = employeeId;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public long getSalary() {
-		return salary;
-	}
-	public void setSalary(long salary) {
-		this.salary = salary;
-	}
-	
 	
 }
